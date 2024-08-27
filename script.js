87b9d3a637ca5d9df4b0b926835bec97bc363895
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-08-27 18:59:09 +0300

    complete console functionality, add existing move check

--- script.js
+++ script.js
@@ -17,15 +17,6 @@ function createGame() {
         gameboard.playerBlog.push(tile);
     }
 
-    function playerTurn() {
-        
-            if (gameboard.playerAlog.length > gameboard.playerBlog.length) {
-                return "B";
-            } else {
-                return "A";
-            }
-    }
-
     function checkTriad(playLog) {
         if (playLog.length < 3) {
             return false;
@@ -46,14 +37,67 @@ function createGame() {
             
         }
     }
-    return {playerAmove,playerBmove,playerTurn,checkTriad};
-}
 
-const newGame = createGame();
-
-function playRound(playerA, playerB) {
+    window.alert("Enter player A details");
+    const playerA = createPlayer();
+    window.alert("Enter player B details");
+    const playerB = createPlayer();
     
+
+    return {playerA,playerB,playerAmove,playerBmove,checkTriad};
 }
+
+function createPlayer() {
+    const name = prompt("Please enter your name: ", "");
+    return {name};
+
+}
+
+
+function playRound() {
+    
+        let playermove;
+        if (gameboard.playerAlog.length > gameboard.playerBlog.length) {
+            playermove = prompt(`It's ${game.playerB.name}'s turn. Enter your move: `, "");
+            if (checkExisting(playermove) === true) {
+                window.alert("That's taken!");
+                playRound();
+            } else {
+                game.playerBmove(playermove);
+                if (game.checkTriad(gameboard.playerBlog) === true) {
+                    window.alert(`${game.playerB.name} wins!`);
+                } else {
+                    playRound();
+                }
+            }
+        } else {
+            playermove = prompt(`It's ${game.playerA.name}'s turn. Enter your move: `, "");
+            if (checkExisting(playermove) === true) {
+                window.alert("That's taken!");
+                playRound();
+            } else {
+                game.playerAmove(playermove);
+                if (game.checkTriad(gameboard.playerAlog) === true) {
+                    window.alert(`${game.playerA.name} wins!`);
+                } else {
+                    playRound();
+                }
+            }
+        }
+        console.log(gameboard.playerAlog);
+        console.log(gameboard.playerBlog);
+     
+}
+
+function checkExisting(move) {
+    if (gameboard.playerAlog.includes(move) || gameboard.playerBlog.includes(move)) {
+        return true;
+    } else return false;
+}
+
+
+const game = createGame();
+playRound();
 
 
 
